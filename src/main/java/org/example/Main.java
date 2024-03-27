c7038b7b195fa45b1f960757fbcaea3023ddcb70
Ravelomanana <ravelomananash@gmail.com>
2024-03-27 13:27:55 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https:google.com");
+        WebElement searchBar = driver.findElement(By.name("q"));
+        searchBar.sendKeys("inclusiv academy");
+        searchBar.submit();
+        driver.findElement(By.cssSelector("h3")).click();
+
+        //driver.quit();
+
+    }
+}
